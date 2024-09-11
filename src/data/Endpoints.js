db6140afb3fda1db74693857280ecac56ed42f46
SzymonM2006 <szymonmrozek191@gmail.com>
2024-09-11 19:06:14 +0200

    easy fake shop project better

--- src/data/Endpoints.js
+++ src/data/Endpoints.js
@@ -4,31 +4,8 @@ const endpoints = {
   getAllProducts: async () => {
     const data = await getData();
     return data;
-  },
-
-  getProductsByCategory: async (category) => {
-    const data = await getData();
-    const productsByCategory = data.filter((product) => product.category === category);
-    return productsByCategory;
-  },
-
-  getProductsByPriceRange: async (minPrice, maxPrice) => {
-    const data = await getData();
-    const productsByPriceRange = data.filter((product) => product.price >= minPrice && product.price <= maxPrice);
-    return productsByPriceRange;
-  },
-
-  getProductsByRating: async (minRating) => {
-    const data = await getData();
-    const productsByRating = data.filter((product) => product.rating.rate >= minRating);
-    return productsByRating;
-  },
-
-  getProductById: async (id) => {
-    const data = await getData();
-    const product = data.find((product) => product.id === id);
-    return product;
   }
+
 };
 
 export default endpoints;
